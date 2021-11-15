6bc4e36e488f458b307fa04906eb4fa308e5acfc
trishateh <trishateh@gmail.com>
2021-11-15 23:12:45 +0800

    First commit

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+        ["Blossom", "Bubbles", "Buttercup"],
+        ["Qmawp8NjDZoo8Gv4KwDH7KVT2fZNeXdat5PNbSyPXGbtYF", "QmWxpHEHuSkqWC7tc6uiEXZeEeZ7DZAhwiUyzjU1vAjtyZ", "QmWN9oqGYgtjm9zwMwCrwb6bN18MeqQy7JeHWBswAvSv7M"],
+        [200, 300, 100],
+        [50, 25, 100],
+        "Mojojojo",
+        "QmbxS4NK8x34J2zMCpRAHiqQgNNt5g5vXrsm7RGBptBqtT",
+        10000,
+        50         
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+    
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
